6676010143b7b1b9c776be109eca1c573a86e14f
nattapong.top <nattapong.top@hotmail.com>
2026-02-25 15:44:04 +0700

    feat: return coffee and the change of 70 baht when buying coffee with 100 baht

--- domain/logic.py
+++ domain/logic.py
@@ -2,8 +2,7 @@ class Cafe:
 
     PRICE = {
         'Tea': 20.0,
-        'Coffee': 30.0
-    }
+        'Coffee': 30.0}
 
     def buy(self, drink: str, money: float) -> tuple[str, float]:
         change = money - self.PRICE[drink]
