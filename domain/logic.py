fdee9a6cd75cd41290ead21a0053bd18338a1ccf
nattapong.top <nattapong.top@hotmail.com>
2026-02-25 16:59:35 +0700

    feat: test should raise DrinkNotInMenu when buying cocoa

--- domain/logic.py
+++ domain/logic.py
@@ -5,6 +5,14 @@ class Cafe:
         'Coffee': 30.0}
 
     def buy(self, drink: str, money: float) -> tuple[str, float]:
+        self._validate_drink(drink)
         change = money - self.PRICE[drink]
         return drink, change
 
+    def _validate_drink(self, drink: str):
+        if drink not in self.PRICE:
+            raise DrinkNotInMenu()
+
+
+class DrinkNotInMenu(Exception):
+    pass
--- test_logic.py
+++ test_logic.py
@@ -1,4 +1,6 @@
-from domain.logic import Cafe
+from _pytest.raises import raises
+from domain.logic import Cafe, DrinkNotInMenu
+
 
 def test_should_return_tea_and_the_change_of_30_bath_when_buying_tea_with_50_baht():
     cafe = Cafe()
@@ -10,3 +12,7 @@ def test_should_return_coffee_and_the_change_of_70_bath_when_buying_coffee_with_
     drink, money = cafe.buy('Coffee', 100.0)
     assert (drink, money) == ('Coffee', 70.0)
 
+def test_should_raise_DrinkNotInMenu_when_buying_cocoa():
+    cafe = Cafe()
+    with raises(DrinkNotInMenu):
+        cafe.buy('Cocoa',50.0)
